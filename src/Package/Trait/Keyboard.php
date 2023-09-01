5039b7670c6cfad28c874effab8132b5b6b71ae5
remco van der Velde <remco.pc@outlook.com>
2023-09-01 14:07:58 +0200

    init application

--- src/Package/Trait/Keyboard.php
+++ src/Package/Trait/Keyboard.php
@@ -1,16 +1,41 @@
 <?php
 namespace Package\R3m\Io\Application\Trait;
 
+use R3m\Io\Module\Data as Storage;
 use R3m\Io\Node\Model\Node;
 
 trait Keyboard {
 
 
     public function list($application){
+        $application = new Storage($application);
         $object = $this->object();
         $node = new Node($object);
+        $class = 'Keyboard';
+        $role = $application->get('#role');
+        $options = [];
+        $options['sort'] = [];
+        $options['sort']['uuid'] = 'ASC';
+        $options['where'] = [];
+        $options['where'][] = [
+            'application' => [
+                'value' => $application->get('uuid'),
+                'operator' => '==='
+            ]
+        ];
+        $options['where'][] = 'and';
+        $options['where'][] = [
+            'host' => [
+                'value' => $application->get('host.uuid'),
+                'operator' => '==='
+            ]
+        ];
+        $count = $node->count($class, $role, $options);
+        ddd($count);
+        $response = $node->list($class, $role, $options);
 
-        d($node);
+
+        d($response);
 //        $count =
 
 
