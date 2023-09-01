4f5e779a76c97f3ba1e45df7f7f56168eaea7579
remco van der Velde <remco.pc@outlook.com>
2023-09-01 13:56:24 +0200

    init application

--- src/Package/Trait/Keyboard.php
+++ src/Package/Trait/Keyboard.php
@@ -1,10 +1,21 @@
 <?php
 namespace Package\R3m\Io\Application\Trait;
 
+use R3m\Io\Node\Model\Node;
+
 trait Keyboard {
 
 
     public function list($application){
+        $object = $this->object();
+        $node = new Node($object);
+
+        d($node);
+//        $count =
+
+
+
+
         ddd($application);
     }
 
